dd4a451220ca8649f544eed6e9fcc654336ea8b8
sakib-xrz <sakibxrz21@gmail.com>
2025-04-03 20:35:06 +0600

    first commit

---
+++ dist/src/app.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+// import router from './app/routes';
+// import notFound from './app/middlewares/notFound';
+// import globalErrorHandler from './app/middlewares/globalErrorHandler';
+const app = (0, express_1.default)();
+app.use((0, morgan_1.default)('dev'));
+app.use(express_1.default.json({ limit: '50mb' }));
+app.use(express_1.default.urlencoded({ extended: true, limit: '50mb' }));
+app.use(body_parser_1.default.urlencoded({ extended: true, limit: '50mb' }));
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)({
+    origin: ['http://localhost:3000', 'https://medimart-self.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept',
+    credentials: true,
+}));
+// application routes
+// app.use('/api/v1', router);
+//global error handler
+// app.use(globalErrorHandler);
+// handle not found routes
+// app.use(notFound);
+exports.default = app;
---
+++ src/server.ts
@@ -0,0 +1,42 @@
+ 
+import { Server } from 'http';
+import app from './app';
+import config from './app/config';
+
+process.on('uncaughtException', (error) => {
+  console.log(
+    'Uncaught Exception! Shutting down the server due to uncaught exception...',
+    error,
+  );
+  process.exit(1);
+});
+
+let server: Server | null = null;
+
+async function startServer() {
+  server = app.listen(config.port, () => {
+    console.log(`🎯 Server listening on port: ${config.port}`);
+  });
+
+  process.on('unhandledRejection', (error) => {
+    if (server) {
+      server.close(() => {
+        console.log(
+          'Unhandled Rejection! Shutting down the server due to unhandled rejection...',
+          error,
+        );
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+startServer();
+
+process.on('SIGTERM', () => {
+  if (server) {
+    server.close();
+  }
+});
